e642c1785bd93a1f96b146219eb760e164b4e548
Strawberry <zjx@stbchain.com>
2020-01-10 11:32:56 +0800

    未登录的情况下调整登录注册页面

--- hxwmi_mobile/src/Views/Index/MyCourse/index.js
+++ hxwmi_mobile/src/Views/Index/MyCourse/index.js
@@ -16,26 +16,90 @@ class MyCourse extends Component {
 
     constructor(props) {
         super(props);
+
+        //定义分页信息
+        this.pageNo = 0;
+
         this.state ={
-            initialPage:0
+            initialPage:0,
+            isLoading:false,
+            hasMore: true,
         }
     }
 
     async componentDidMount(){
-
+        let res = await this.GetCourseList();
+        console.log(res);
     }
 
     GetCourseList = async ()=>{
+        this.pageNo++;
+
         let params = {
             user_token:'',
             show_type:'1',  //显示类型 1:公开课   2:专题班   3:系统班  4:直播课
-            page:'1',
-            limit:'10'
+            page: this.pageNo,
+            limit:'5'
         }
+
+        this.setState({
+            isLoading:true
+        });
+
         let res = await GetMyCourseList_Async(JSON.stringify(params));
+
+        this.setState({
+            isLoading:false
+        });
+
+        if(res && res.code === "200" && res.msg === "success"){
+            return res.data;
+        }
+        
+        return null;
+    }
+
+    ScrollHandler = async (e)=>{
+        let dom = e.target;
+
+        let position = {
+            top: dom.scrollTop,
+            left: dom.scrollLeft,
+            width: dom.scrollWidth,
+            height: dom.scrollHeight,
+            clientHeight: dom.clientHeight  //div的可视区域
+        };
+        
+        console.log('offset:',position);
+
+        //下拉到最底部加载下一页
+        if((position.height - position.top <= dom.clientHeight) && (this.state.isLoading === false)){
+            await this.onRefresh();
+        }
+    }
+
+    //刷新加载数据
+    onRefresh = async ()=>{
+        
+        let res = await this.GetCourseList();
+        console.log(res);
+
+        if(!res){
+            this.props.history.push('/login');
+        }
+
+        if(res && res.length === 0){
+            //暂无更多数据
+            this.setState({
+                hasMore:false
+            });
+        }
+
     }
     
     render() {
+        console.log(this.props);
+        
         return (
             <div className="my_Course_index">
 
@@ -51,13 +115,14 @@ class MyCourse extends Component {
                         animated
                         tabs={tabs}
                         initialPage={ this.state.initialPage }
-                        onTabClick={(index) => { 
+                        onTabClick={(tab,index) => {
+                            console.log(tab); 
                             this.setState({
                                 initialPage:index
                             });
                         }}
                     >
-                        <div className="course_tab_item">
+                        <div className="course_tab_item" onScroll={ this.ScrollHandler }>
 
                             <div className="my_course_item">
                                 <div className="my_course_item_left">
@@ -71,6 +136,15 @@ class MyCourse extends Component {
                                 <div className="my_course_item_right">
                                     <a>立即学习</a>
                                 </div>
+                            </div>
+
+                            <div 
+                                className="mycourse_listview_footer" 
+                                onClick={ this.onRefresh }
+                            >
+                                {
+                                    this.state.hasMore ?"点击或下拉加载数据...":"暂无更多数据"
+                                }
                             </div>
 
                         </div>
